d22a80ea78f229e8f2d04ab95f8b6d9cc04342fa
Oscar Laird <olaird25@gmail.com>
2024-04-07 16:32:30 -0400

    wait for pinecone to finish building in CREATE

--- src/pinecone/pinecone_build.c
+++ src/pinecone/pinecone_build.c
@@ -91,7 +91,17 @@ IndexBuildResult *pinecone_build(Relation heap, Relation index, IndexInfo *index
         result->heap_tuples = 0;
         result->index_tuples = 0;
     } else {
+        cJSON* index_stats_response;
         InsertBaseTable(heap, index, indexInfo, host, result);
+        // wait for the remote index to finish processing the vectors
+        // i.e. describe stats is equal to result->index_tuples
+        index_stats_response = pinecone_get_index_stats(pinecone_api_key, host);
+        while (cJSON_GetObjectItemCaseSensitive(index_stats_response, "totalVectorCount")->valueint < result->index_tuples) {
+            elog(DEBUG1, "Waiting for remote index to process vectors...");
+            elog(DEBUG1, "Got response: %s", cJSON_Print(index_stats_response));
+            sleep(1);
+            index_stats_response = pinecone_get_index_stats(pinecone_api_key, host);
+        }
     }
     return result;
 }
